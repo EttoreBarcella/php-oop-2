a488d49dfdb8e85f712b7edf006a6a1c03bad1fb
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-29 01:44:43 +0200

    added product

--- classes/Product.php
+++ classes/Product.php
@@ -1,30 +1,17 @@
 <?php
 
-class CreditCard {
-    public $number;
-    public $expire_date;
-    public $brand;
+class Product {
     public $name;
-    public $surname;
-    public $cvc;
+    public $price;
+    public $quantity;
+    public $animal_type;
 
-    public function __construct(int $_number, string $_expire_date, string $_brand, string $_name, string $_surname, int $_cvc){
-        $this->number = $_number;
-        $this->expire_date = $_expire_date;
-        $this->brand = $_brand;
+    public function __construct(string $_name, string $_price, string $_quantity, string $animal_type)
+    {
         $this->name = $_name;
-        $this->surname = $_surname;
-        $this->cvc = $_cvc;
-    }
-    // function expired card
-    public function isExpired($creditCardData){
-
-        $flag = false;
-
-        if(intval(substr($creditCardData, -4)) >=  intval(date("Y")) && intval(substr($creditCardData, -7, 2)) >=  intval(date("m")) && intval(substr($creditCardData, -10, 2)) >  intval(date("d"))){
-            $flag = true;
-        }
-
-        return $flag;
+        $this->price = $_price;
+        $this->quantity = $_quantity;
+        $this->animal_type = $_animal_type;
+        
     }
 }
