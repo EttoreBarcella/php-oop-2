a272bfb970f5a966d800d9df1f02fedb213ec36a
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-29 09:15:41 +0200

    added food + toy

---
+++ classes/Toy.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/Product.php'
+
+class Toy extends Product {
+    public $type;
+    public $size;
+
+    public function __construct(string $_name, float $_price, int $_quantity, string $_animal_type, string $_type, string $_size){
+
+        parent::__construct($_name, $_price, $_quantity, $_animal_type);
+
+        $this->type = $_type;
+        $this->size = $_size;
+    }
+}
